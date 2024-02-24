7bcb20ad7ead251fb11477c335b47db689cf9146
unknown <siminchan312@gmail.com>
2024-02-24 22:19:37 +0800

    added all common function in the util file.

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -1,38 +1,41 @@
 package base;
 
+import driver.AppDriver;
+import driver.AppFactory;
+import org.testng.ITest;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+import org.testng.annotations.BeforeMethod;
+
 import java.io.IOException;
 import java.net.MalformedURLException;
 
-import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
-
-import bsh.util.Util;
-import driver.AppDriver;
-import driver.AppFactory;
-
 public class BaseTest {
 
-	@BeforeClass
-    public void launchApp() throws MalformedURLException {
-        System.out.println("before method");
-        AppFactory.launchApp();
-        //base.AppiumServer.start();
-    }
+	 @BeforeClass
+	    public void launchApp() throws MalformedURLException {
+	        System.out.println("before method");
+	        AppFactory.launchApp();
+	        //base.AppiumServer.start();
+	    }
 
+	    @AfterMethod
+	    public void closeApp(ITestResult result) throws IOException {
+	        if(result.getStatus() == ITestResult.FAILURE){
+	            Util.getScreenshot(result.getTestName());
+	        }
+	        //AppDriver.getCurrentDriver().quit();
+	        //base.AppiumServer.stop();
+	    }
 
-    @BeforeSuite
-    public void serverStart(){
-        System.out.println("before suite");
-        base.AppiumServer.start();
-    }
+	    @BeforeSuite
+	    public void serverStart(){
+	        System.out.println("before suite");
+	        base.AppiumServer.start();
+	    }
 
-    @AfterSuite
-    public void serverStop(){
-        base.AppiumServer.stop();
-    }
+	    @AfterSuite
+	    public void serverStop(){
+	        base.AppiumServer.stop();
+	    }
 }
